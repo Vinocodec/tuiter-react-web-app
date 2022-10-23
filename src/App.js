3ff33094f63200fd647be2ebb4fee2def205d801
Vinocodec <91758525+Vinocodec@users.noreply.github.com>
2022-10-23 16:44:28 -0700

    a6 React.js fall 2022

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HelloWorld from './labs';
+import HelloWorld from './labs/a6/hello-world';
 import Labs from './labs';
 import Tuiter from "./components/tuiter/tuiter";
 import HomeScreen from './components/tuiter/HomeScreen';
@@ -12,9 +12,12 @@ function App() {
         <BrowserRouter>
             <div className="container">
                 <Routes>
-                    <Route path="/hello" exact={true} element={<HelloWorld/>}/>
-                    <Route path="/" exact={true} element={<Labs/>}/>
-                    <Route path="/tuiter" exact={true} element={<Tuiter/>}/>
+                    <Route index
+                           element={<Labs/>}/>
+                    <Route path="/hello"
+                           element={<HelloWorld/>}/>
+                    <Route path="/tuiter/*"
+                           element={<Tuiter/>}/>
                     <Route path="/tuiter/home" exact={true} element={<HomeScreen/>}/>
                     <Route path="/tuiter/explore" exact={true} element={<ExploreScreen/>}/>
                 </Routes>
