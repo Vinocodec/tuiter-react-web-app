d34e36bdc34e826b5f1962f87622bd079a869958
Vinocodec <91758525+Vinocodec@users.noreply.github.com>
2022-10-23 16:21:05 -0700

    a6 React.js fall 2022

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HelloWorld from './components/hello-world';
+import Labs from './labs';
+import Tuiter from "./components/tuiter/tuiter";
+import HomeScreen from './components/tuiter/HomeScreen';
+import ExploreScreen from './components/tuiter/ExploreScreen';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="container">
+                <Routes>
+                    <Route path="/hello" exact={true} element={<HelloWorld/>}/>
+                    <Route path="/" exact={true} element={<Labs/>}/>
+                    <Route path="/tuiter" exact={true} element={<Tuiter/>}/>
+                    <Route path="/tuiter/home" exact={true} element={<HomeScreen/>}/>
+                    <Route path="/tuiter/explore" exact={true} element={<ExploreScreen/>}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
+
+
